9df7c0ea221822b4e75cad3fc1185098eadf8286
Christian <cjfuentes27@ieszaidinvergeles.org>
2022-11-14 00:52:45 +0100

    Reserve calendar

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuItems = document.querySelector('.menu-items');
+const days = document.querySelectorAll('td');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('cross');
     menuItems.classList.toggle('open');
+});
+
+days.forEach(day => {
+    if (day.children.length != 0) {
+        day.addEventListener('click', () => {
+            day.children[0].checked = true;
+        });
+    }
 });
