e5c9c345f2ddd31447136e706a96334d9a33763b
Christian Fuentes <cjfuentes27@ieszaidinvergeles.org>
2022-11-14 12:43:41 +0100

    Contact section mobile

--- js/contact.js
+++ js/contact.js
@@ -1,17 +1,10 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menuItems = document.querySelector('.menu-items');
 const substractBtn = document.querySelector('.substract-btn');
 const addBtn = document.querySelector('.add-btn');
 const guests = document.querySelector('.guest-number');
-const days = document.querySelectorAll('td');
 const hourItem = document.querySelectorAll('.hour-item');
+const days = document.querySelectorAll('td');
 
 let number = guests.innerHTML;
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('cross');
-    menuItems.classList.toggle('open');
-});
 
 addBtn.addEventListener('click', () => {
     if (number < 20) {
