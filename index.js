b13b8a661d69bc0623ec23755b961f0fa73c1646
Christian Fuentes <cjfuentes27@ieszaidinvergeles.org>
2022-11-14 11:14:39 +0100

    Reserve mobile

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuItems = document.querySelector('.menu-items');
+const substractBtn = document.querySelector('.substract-btn');
+const addBtn = document.querySelector('.add-btn');
+const guests = document.querySelector('.guest-number');
 const days = document.querySelectorAll('td');
+const hourItem = document.querySelectorAll('.hour-item');
+
+let number = guests.innerHTML;
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('cross');
     menuItems.classList.toggle('open');
+});
+
+addBtn.addEventListener('click', () => {
+    if (number < 20) {
+        number++;
+        guests.innerHTML = number;
+    }
+});
+
+substractBtn.addEventListener('click', () => {
+    if (number > 0) {
+        number--;
+        guests.innerHTML = number;
+    }
 });
 
 days.forEach(day => {
@@ -13,4 +33,8 @@ days.forEach(day => {
             day.children[0].checked = true;
         });
     }
+});
+
+hourItem.forEach(hour => {
+    hour.addEventListener('click', () => hour.children[0].checked = true);
 });
