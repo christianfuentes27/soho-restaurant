b40083d8d8731040dcd1ec74fc6ebd42b7e5e02d
Christian Fuentes <cjfuentes27@ieszaidinvergeles.org>
2022-11-14 13:41:29 +0100

    Fix menu mobile

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menuItems = document.querySelector('.menu-items');
+const menu = document.querySelectorAll('.navbar a');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('cross');
     menuItems.classList.toggle('open');
+    for (let a of menu) {
+        if (a.style.display === "block") {
+            a.style.display = "none";
+        } else {
+            a.style.display = "block";
+        }
+    }
 });
